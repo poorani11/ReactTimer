893165a193f33043ef6b5562595413c8bf6d64e3
poorani11 <poorani.mgr@gmail.com>
2018-06-09 18:06:44 +0530

    Intial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Main = (props) => {
+    return (
+        <div>
+          <h1 className="text-center">Main.jsx rendered</h1>
+        </div>
+    )
+};
+
+module.exports = Main;
